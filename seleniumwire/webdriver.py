480f812e49198670a1b4cefff1cdcf0cf1badfb6
SunnyCapt <sunny.capt@tuta.io>
2021-04-27 16:51:14 +0300

    added the ability to completely disable selenium-wire and use selenium

--- seleniumwire/webdriver.py
+++ seleniumwire/webdriver.py
@@ -30,6 +30,12 @@ from seleniumwire.server import MitmProxy
 
 SELENIUM_V4 = parse_version(getattr(selenium, '__version__', '0')) >= parse_version('4.0.0')
 
+try:
+    # noinspection PyUnresolvedReferences
+    from undetected_chromedriver import ChromeOptions
+except ImportError:
+    pass
+
 
 class DriverCommonMixin:
     """Attributes common to all webdriver types."""
@@ -140,8 +146,8 @@ class DriverCommonMixin:
             )
 
 
-class Firefox(InspectRequestsMixin, DriverCommonMixin, _Firefox):
-    """Extend the Firefox webdriver to provide additional methods for inspecting requests."""
+class _SeleniumWireFirefox(InspectRequestsMixin, DriverCommonMixin, _Firefox):
+    """Extends the Firefox webdriver to provide additional methods for inspecting requests."""
 
     def __init__(self, *args, seleniumwire_options=None, mitm_proxy: MitmProxy = None, **kwargs):
         """Initialise a new Firefox WebDriver instance.
@@ -197,8 +203,8 @@ class Firefox(InspectRequestsMixin, DriverCommonMixin, _Firefox):
         super().__init__(*args, **kwargs)
 
 
-class Chrome(InspectRequestsMixin, DriverCommonMixin, _Chrome):
-    """Extend the Chrome webdriver to provide additional methods for inspecting requests."""
+class _SeleniumWireChrome(InspectRequestsMixin, DriverCommonMixin, _Chrome):
+    """Extends the Chrome webdriver to provide additional methods for inspecting requests."""
 
     def __init__(self, *args, seleniumwire_options=None, mitm_proxy: MitmProxy = None, **kwargs):
         """Initialise a new Chrome WebDriver instance.
@@ -234,8 +240,8 @@ class Chrome(InspectRequestsMixin, DriverCommonMixin, _Chrome):
         super().__init__(*args, **kwargs)
 
 
-class Safari(InspectRequestsMixin, DriverCommonMixin, _Safari):
-    """Extend the Safari webdriver to provide additional methods for inspecting requests."""
+class _SeleniumWireSafari(InspectRequestsMixin, DriverCommonMixin, _Safari):
+    """Extends the Safari webdriver to provide additional methods for inspecting requests."""
 
     def __init__(self, seleniumwire_options=None, *args, mitm_proxy: MitmProxy = None, **kwargs):
         """Initialise a new Safari WebDriver instance.
@@ -263,8 +269,8 @@ class Safari(InspectRequestsMixin, DriverCommonMixin, _Safari):
         super().__init__(*args, **kwargs)
 
 
-class Edge(InspectRequestsMixin, DriverCommonMixin, _Edge):
-    """Extend the Edge webdriver to provide additional methods for inspecting requests."""
+class _SeleniumWireEdge(InspectRequestsMixin, DriverCommonMixin, _Edge):
+    """Extends the Edge webdriver to provide additional methods for inspecting requests."""
 
     def __init__(self, seleniumwire_options=None, *args, mitm_proxy: MitmProxy = None, **kwargs):
         """Initialise a new Edge WebDriver instance.
@@ -290,8 +296,8 @@ class Edge(InspectRequestsMixin, DriverCommonMixin, _Edge):
         super().__init__(*args, **kwargs)
 
 
-class Remote(InspectRequestsMixin, DriverCommonMixin, _Remote):
-    """Extend the Remote webdriver to provide additional methods for inspecting requests."""
+class _SeleniumWireRemote(InspectRequestsMixin, DriverCommonMixin, _Remote):
+    """Extends the Remote webdriver to provide additional methods for inspecting requests."""
 
     def __init__(self, *args, seleniumwire_options=None, mitm_proxy: MitmProxy = None, **kwargs):
         """Initialise a new Firefox WebDriver instance.
@@ -320,3 +326,62 @@ class Remote(InspectRequestsMixin, DriverCommonMixin, _Remote):
             kwargs['desired_capabilities'] = capabilities
 
         super().__init__(*args, **kwargs)
+
+
+class Firefox:
+    def __new__(cls, *args, use_seleniumwire=True, **kwargs):
+        clazz = _SeleniumWireFirefox if use_seleniumwire else _Firefox
+        return clazz(*args, **kwargs)
+
+
+class Chrome:
+    def __new__(cls, *args, use_seleniumwire=True, **kwargs):
+        clazz = _SeleniumWireChrome if use_seleniumwire else _Chrome
+
+        try:
+            # noinspection PyUnresolvedReferences
+            import undetected_chromedriver as uc
+
+            if 'chrome2use' not in kwargs:
+                kwargs['chrome2use'] = clazz
+
+            clazz = uc.Chrome
+        except ImportError:
+            pass
+
+        return clazz(*args, **kwargs)
+
+
+class Safari:
+    def __new__(cls, *args, use_seleniumwire=True, **kwargs):
+        clazz = _SeleniumWireSafari if use_seleniumwire else _Safari
+        return clazz(*args, **kwargs)
+
+
+class Edge:
+    def __new__(cls, *args, use_seleniumwire=True, **kwargs):
+        clazz = _SeleniumWireEdge if use_seleniumwire else _Edge
+        return clazz(*args, **kwargs)
+
+
+class Remote:
+    def __new__(cls, *args, use_seleniumwire=True, **kwargs):
+        clazz = _SeleniumWireRemote if use_seleniumwire else _Remote
+        return clazz(*args, **kwargs)
+
+
+def urlsafe_address(address):
+    """Make an address safe to use in a URL.
+
+    Args:
+        address: A tuple of address information.
+    Returns:
+        A 2-tuple of url-safe (address, port)
+    """
+    addr, port, *rest = address
+
+    if rest:
+        # An IPv6 address needs to be surrounded by square brackets
+        addr = f'[{addr}]'
+
+    return addr, port
